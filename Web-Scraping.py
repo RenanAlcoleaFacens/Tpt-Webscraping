dba08a34f1ac0d74c8ad1f5cf0f7e557c20bea28
Lucca Lo Presti <lucca.campos@facens.br>
2022-05-19 10:20:54 -0300

    teste

--- Web-Scraping.py
+++ Web-Scraping.py
@@ -1,52 +1,22 @@
+'''
 from fileinput import filename
 from math import fabs
 from re import A
 from attr import attrs
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from time import sleep
+from numpy import empty
+from datetime import datetime
 import pandas as pd
 import os
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email.message import EmailMessage
-from email import encoders
-from tiger_pass import senha
+from bs4 import BeautifulSoup
+from time import sleep
+from attr import attrs
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from datetime import date
+from selenium.webdriver.chrome.options import Options
+from tiger_pass import senha
+from funcoes import *
 import re
-from openpyxl import workbook,load_workbook
-
-options = Options()
-options.add_argument('window-size=800,1200')
-#options.add_argument("--headless")
-
-home = ("https://nvd.nist.gov/")
-navegador = webdriver.Chrome(options=options)
-navegador.get('https://nvd.nist.gov/vuln/search')
-
-sleep(2)
-
-site = BeautifulSoup(navegador.page_source, 'html.parser')
-caixa_advanced = navegador.find_element(By.ID, 'SearchTypeAdvanced')
-caixa_advanced.click()
-
-sleep(2)
-
-cve_input = navegador.find_element(By.ID, "Keywords")
-cve_informado = input("Digite a CVE desejada para pesquisa: ")
-cve_informado=cve_informado.upper()
-cve_input.send_keys(cve_informado)
-
-data_inicio = navegador.find_element(By.ID, "published-start-date")
-data_informada_inicio = input("Informe a data de início [mm/dd/yyyy]: ")
-data_inicio.send_keys(data_informada_inicio)
-data_fim = navegador.find_element(By.ID, "published-end-date")
-data_informada_fim = input("Informe a data de término [mm/dd/yyyy]: ")
-data_fim.send_keys(data_informada_fim)
+from datetime import date
 
 #Validar email
 padrao = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
@@ -55,103 +25,171 @@ while True:
     if (re.search(padrao,email_informado)) :
         print("Email válido")
         break
-    
-pesquisar = navegador.find_element(By.ID, "vuln-search-submit")
-pesquisar.click()
+'''
 
-sleep(2)
+#software = input('Digite qual a vulnerabilidade que gostaria de procurar: ')
+#startDate_input = input('Informe qual a data de Início: ')
+#startDate_input = '02/01/2022'
+#endDate_input = input('Informe qual a data Final: ')
+#endDate_input = '02/07/2022'
+#email_informato = input("Digite um e-mail válido para receber as CVEs encontradas: ")
 
-page_content = navegador.page_source
-site = BeautifulSoup(page_content, 'html.parser')
-dados_scraping = []
-inicio_contagem = site.find('strong', attrs={'data-testid': 'vuln-displaying-count-from'}).text
-inicio_contagem = int(inicio_contagem) - 1
-fim_contagem = site.find('strong', attrs={'data-testid': 'vuln-displaying-count-through'}).text
-fim_contagem = int(fim_contagem) - 1
-numero = 0
+from ssl import AlertDescription
+import pandas as pd
+from bs4 import BeautifulSoup
+from time import sleep
+from attr import attrs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from tiger_pass import senha
+from funcoes import *
+from flask import Flask, render_template, flash
+from flask import request
+from datetime import datetime
+import pyautogui as pag
 
-print("Iniciando a pesquisa...")
+app = Flask(__name__)
 
-while numero <= fim_contagem:
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
 
-    
-    cves = site.find('tr', attrs={'data-testid': 'vuln-row-' + str(numero)})
-    severity = cves.find('a', attrs={'data-testid': 'vuln-cvss3-link-' + str(numero)})
-    if (severity):
-        severity = (severity.text).split(" ")[0]
-        #severity = float(severity)
+@app.route("/pesquisar", methods=['POST'])
+def pesquisar():
+    error = None
+    try:
 
-    cves = site.find('tr', attrs={'data-testid': 'vuln-row-' + str(numero)})
-    titulo = cves.find('a', attrs={'data-testid': 'vuln-detail-link-' + str(numero)})
-    link = cves.find('a', attrs={'data-testid': 'vuln-detail-link-' + str(numero)})
-    descricao = cves.find('p', attrs={'data-testid': 'vuln-summary-' + str(numero)})
-    data = cves.find('span', attrs={'data-testid': 'vuln-published-on-' + str(numero)})
-    link_completo = home+link['href']
-    
+        software_flask = request.form['software']
+        email_flask = request.form['email']
+        data_inicio_flask = request.form['dataInicio']
+        data_termino_flask = request.form['dataTermino']        
+
+        datas = validador_datas(data_inicio_flask, data_termino_flask)
+        startDate_input = datas[0]
+        endDate_input = datas[1]    
+
+        #Parâmetros de Opções do Webdriver do Chrome 
+        options = Options()
+        options.add_argument('window-size=800,1000')
+        options.add_argument('--headless')
+
+        #Definindo o navegador através do Webdriver e inicializando com parâmetros de um objetos da classe "Option"
+        navegador = webdriver.Chrome(options=options)
+        navegador.get('https://nvd.nist.gov/vuln/search')
+
+        #Sleep de 2 segundos para renderizar toda a página
+        sleep(2)
+
+        #Seleciona o tipo de Busca como Avançado
+        advanced = navegador.find_element(By.ID,'SearchTypeAdvanced')
+        advanced.click()
+
+        sleep(2)
+
+        #Seleciona a caixa de pesquisa da vulnerabilidade e digita a String passada pelo usuário
+        software_flask =software_flask.upper()
+        keywords = navegador.find_element(By.ID,'Keywords')
+        keywords.send_keys(software_flask)       
+
+        #Definindo Range de Início da busca
+        startDate = navegador.find_element(By.ID,'published-start-date')        
+        startDate.send_keys(startDate_input)
+
+        #Definindo Range Final da busca
+        endDate = navegador.find_element(By.ID,'published-end-date')        
+        endDate.send_keys(endDate_input)
+
+        #Selecionando o botão de Search
+        submitBtn = navegador.find_element(By.ID,'vuln-search-submit')
+        submitBtn.click()
+
+        sleep(2)
+
+        #Transformando o conteúdo da página no padrão do Beautiful Soup 4
+        siteFP = BeautifulSoup(navegador.page_source,'html.parser')
+        tableContent = siteFP.find('table', attrs={'data-testid': 'vuln-results-table'})
+        headContent =  siteFP.find('div', attrs={'id': 'body-section'})
 
 
-    # Clicou em cima da CVE e vai nos detalhes da CVE
-    segunda_pag = navegador.find_element(by=By.LINK_TEXT, value=titulo.text)
-    segunda_pag.click()
+        listFull = []
 
-    #Itens da página da CVE específica
-    navegador_url = navegador.current_url
-    site_page_2 = BeautifulSoup(page_content, 'html.parser')
-    hyperlink = site_page_2.find('tr', attrs={'data-testid': 'vuln-hyperlinks-row-0'})
+        #Verifica a quantidade de páginas das CVE
+        qtd_result = int(siteFP.find('strong',attrs={'data-testid':'vuln-matching-records-count'}).getText())
+        if qtd_result%20 > 0:
+            pages = (qtd_result//20)+1
+        else:
+            pages = qtd_result//20        
 
+        ########################################################################################################################################################
+        for j in range (pages):
 
-    
-    #known = navegador.find_element('b', attrs={'data-testid': 'vuln-software-cpe-1-0-0'})
-    #known = ""
-    navegador.back()
-    
-    numero= numero+1
+            newsiteFP = BeautifulSoup(navegador.page_source,'html.parser')
+            
+            # Calcula a quantidade de itens da página atual
+            if qtd_result <= 20:
+                fim_contagem = qtd_result
+            elif j == pages-1:
+                fim_contagem = qtd_result - 20 * j        
+            else:
+                fim_contagem = 20    
+            
+            for i in range (fim_contagem):                  
+                
+                tableContent = newsiteFP.find('table', attrs={'data-testid': 'vuln-results-table'})
+                headContent =  newsiteFP.find('div', attrs={'id': 'body-section'})
 
-    dados_scraping.append([cve_informado,titulo.text, descricao.text ,severity, hyperlink,"", data.text,link_completo])
+                #Criação da Lista de CVE e Informações
+                listResult =[]
+                
+                if  headContent.find('strong',attrs={'data-testid': 'vuln-matching-records-count'}).get_text() == '0':
+                    print('Não existem falhas para este período informado')
 
-dados = pd.DataFrame(dados_scraping, columns=['Software/Sistema','CVE','Current Description','Severity','References to Advisories,Solutions, and Tools','Know Affected Software Configurations','NVD Published Date','Link para o respectivo CVE'])
-dados.to_excel('webScraping.xlsx', index=False)
+                else:
+                    #Obtendo segundo elemento da Lista final (CVE)
+                    cveInput = tableContent.find('a',attrs={'data-testid': 'vuln-detail-link-'+str(i)}).getText()
 
-print("Pesquisa concluída!\nEstamos enviando as informações para o email informado")
+                    #Obtendo o terceiro elemento da Lista final (Descrição)
+                    descInput = tableContent.find('p',attrs={'data-testid': 'vuln-summary-'+str(i)}).getText()
 
-#Tabela para o Body do email
-tabela=dados.copy()
-segundo_excell = pd.DataFrame(tabela, columns=['Software/Sistema','CVE','Severity','NVD Published Date','Link para o respectivo CVE'])
-segundo_excell.to_excel('webscrap.xlsx', index=False)
-tabela = pd.read_excel("webscrap.xlsx")
+                    #Obtendo o quarto elemento da Lista final (Severidade)            
+                    navegador.get('https://nvd.nist.gov/vuln/detail/'+cveInput)
+                    sleep(2)
+                    siteSP = BeautifulSoup(navegador.page_source,'html.parser')
+                    severity_Input=float(busca_severity(siteSP)) 
+                    
 
-'''planilha=load_workbook("webscrap.xlsx")
-aba_ativa=planilha.active
-for celula in aba_ativa["C"]:
-    if (celula.value) >=7:
-        linha=celula.row
-    else:
-        continue
-planilha.save("webscrap.xlsx", index=False)    '''    
+                    #Obtendo o quinto elemento da Lista final (Referências)
+                    reference_Input = busca_links(siteSP)
+                    #Obtendo o sexto elemento da Lista final (Knowledge Affected System)
+                    kasc_Input = busca_kasc(siteSP)  
+                    #Obtendo o setimo elemento da Lista final (Data de Publicação)
+                    publish_Input = busca_publish(siteSP)
+                    #Obtendo o oitavo elemento da Lista final (Link CVE)
+                    details_Input = busca_details(cveInput)
+                    
+                    listResult = [software_flask,cveInput,descInput,severity_Input,reference_Input,kasc_Input,publish_Input,details_Input]        
+                    listFull.append(listResult)           
+                    navegador.back() 
+            
+            if j < pages-1 and qtd_result >20:
+                nextBtn = navegador.find_element(By.LINK_TEXT,'>')
+                nextBtn.click()
+            
 
-#Configurar e-mail e senha
-EMAIL_ADDRESS = 'timetigerpython@gmail.com'
-EMAIL_PASSWORD = senha
-fromaddr = EMAIL_ADDRESS
-toaddr = email_informado
-msg = MIMEMultipart()
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = ("Vulnerabilidades Críticas, Data: ")
-body = (f"Segue na tabela abaixo as vulnerabilidades classificadas como altas:\n\n{tabela}")
-msg.attach(MIMEText(body, 'plain'))
-filename = "webScraping.xlsx"
-attachment = open("C:\\Users\lenovo\\Documents\\WebScraping\\Desafio Python Web Scraping\\webScraping.xlsx", "rb")
-p = MIMEBase('application', 'octet-stream')
-p.set_payload((attachment).read())
-encoders.encode_base64(p)
-p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-msg.attach(p)
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-s.login(fromaddr, senha)
-text = msg.as_string()
-s.sendmail(fromaddr, toaddr, text)
-s.quit()
+        ########################################################################################################################################################
+
+        #Envio do Email
+        envia_email(listFull,email_flask)
+    except:
+        #return render_template("homepage.html", error = error)
+        pag.alert(text="Por favor, preencha novamente os campos.", title="Erro" )
+        return render_template("homepage.html")
+
+#colocar o site no ar
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.run(debug=True)
+
 
 
--- funcoes.py
+++ funcoes.py
@@ -1,21 +1,25 @@
 from datetime import datetime
+import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.message import EmailMessage
+from email import encoders
+import smtplib
+from h11 import Data
+import pandas as pd
+from tiger_pass import senha    
 
-#Função que retorna a Severity do CVE (4º Item da lista)
-
-#Recebendo os dois inpusts como argumento:
-def validador_datas(dataBR1, dataBR2):
+def validador_datas(dataBR1,dataBR2):
     
-    '''dataBR1 = '01/05/2022'
-    dataBR2 = '16/10/2023'''
+    #dataBR1 = '01/02/2022'
+    #dataBR2 = '10/02/2022'
 
     # Data Inicial
     d1 = datetime.strptime(dataBR1, '%Y-%m-%d')
 
     # Data Final
     d2 = datetime.strptime(dataBR2, '%Y-%m-%d')
-
-    #2022/10/04
-
 
     #Calcula se o período informado é maior que 180 dias:
     if abs((d2 - d1).days)>180:        
@@ -27,11 +31,19 @@ def validador_datas(dataBR1, dataBR2):
         dataUS = [dataUS1,dataUS2]
     return dataUS
 
+#Função que retorna a Severity do CVE (4º Item da lista)
 def busca_severity(siteSP):    
     if siteSP.find('a',attrs={'id': 'Cvss3NistCalculatorAnchor'}):
-        severity_Input = siteSP.find('a',attrs={'id': 'Cvss3NistCalculatorAnchor'}).getText()        
+        severity_Input = siteSP.find('a',attrs={'id': 'Cvss3NistCalculatorAnchor'}).getText() 
+        severity_Input = str(severity_Input).split(" ")[0]   
+        severity_Input = float(severity_Input)
+    elif siteSP.find('a',attrs={'id': 'Cvss3CnaCalculatorAnchor'}):
+        #severity_Input = siteSP.find(attrs={'data-testid':'vuln-cvss3-cna-panel-score'}).getText()
+        severity_Input = siteSP.find('a',attrs={'id': 'Cvss3CnaCalculatorAnchor'}).getText() 
+        severity_Input = str(severity_Input).split(" ")[0]   
+        severity_Input = float(severity_Input)  
     else:
-        severity_Input = 'N/A' 
+        severity_Input = 0
     return severity_Input 
 
 #Função que retorna os Hyperlinks do CVE (5º Item da lista)
@@ -45,7 +57,7 @@ def busca_links(pagina_resultadoCVE_BS,links_impresso = 0,i=0,counter=0):
 
     #Executando o comando de separar o link do corpo html através do get_text(), repetindo a qtd de vezes necessária:
     else:
-        links_impresso = links_impresso + ', ' + str(pagina_resultadoCVE_BS.find('td',attrs={'data-testid':'vuln-hyperlinks-link-'+str(i)}).get_text())
+        links_impresso = links_impresso + ',' + str(pagina_resultadoCVE_BS.find('td',attrs={'data-testid':'vuln-hyperlinks-link-'+str(i)}).get_text())
 
     i=i+1
 
@@ -89,3 +101,49 @@ def busca_publish(siteSP):
 #Função que retorna o Link de Detalhes da CVE (8º Item da lista)
 def busca_details(cveInput):
     return 'https://nvd.nist.gov/vuln/detail/'+cveInput
+
+def envia_email(listFull,email_informado):
+
+    #Montando a estrutura do Dataframe com Pandas
+    df = pd.DataFrame(data = listFull,columns=['Software/Sistema','CVE','Current Description', 'Severity',
+    'References to Advisories, Solutions, and Tools','Known Affected Softwares Configuration','NVD Published Date','Link para o respectivo CVE'])
+
+    #Gerando o arquivo do Excel a partir do Dataframe
+    df.to_excel('Vulnerabilidades_CVE.xlsx',sheet_name='Vulnerabilidades - CVE',header=True,index=False)
+
+    tabela=df.copy()
+    segundo_excell = pd.DataFrame(tabela, columns=['Software/Sistema','CVE','Severity','NVD Published Date','Link para o respectivo CVE'])
+    segundo_excell.to_excel('webscrap.xlsx', index=False)
+    tabela = pd.read_excel("webscrap.xlsx")
+
+    #Retira valores menores que 7 da tabela
+    tabela.loc[tabela["Severity"]<7 ,['Software/Sistema','CVE','Severity','NVD Published Date','Link para o respectivo CVE']]= None
+    tabela = pd.DataFrame(tabela.dropna(how="any"))
+    print("Pesquisa concluída!\nEstamos enviando as informações para o email informado")
+    
+
+    EMAIL_ADDRESS = 'timetigerpython@gmail.com'
+    EMAIL_PASSWORD = senha
+    fromaddr = EMAIL_ADDRESS
+    toaddr = email_informado
+    data=datetime.datetime.now()
+    today=(str(data.day) +"/"+ str(data.month) +"/"+ str(data.year))
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = ("Vulnerabilidades Críticas, Data: ") + today
+    body = (f"Segue na tabela abaixo as vulnerabilidades classificadas como altas:\n\n{tabela}")
+    msg.attach(MIMEText(body, 'plain'))
+    filename = "Vulnerabilidades_CVE.xlsx"
+    attachment = open("Vulnerabilidades_CVE.xlsx", "rb")
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(p)
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(fromaddr, senha)
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    s.quit()
