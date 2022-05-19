c3a6738ea4e791e5b06980b76e06eb6cc003a132
Lucca Lo Presti <lucca.campos@facens.br>
2022-05-19 11:18:49 -0300

    Atualizacao Front

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-echo "web: gunicorn app:app" > Procfile
+echo "web: gunicorn app:Web-Scraping" > Procfile
--- Web-Scraping.py
+++ Web-Scraping.py
@@ -1,39 +1,3 @@
-'''
-from fileinput import filename
-from math import fabs
-from re import A
-from attr import attrs
-from numpy import empty
-from datetime import datetime
-import pandas as pd
-import os
-from bs4 import BeautifulSoup
-from time import sleep
-from attr import attrs
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from tiger_pass import senha
-from funcoes import *
-import re
-from datetime import date
-
-#Validar email
-padrao = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-while True:
-    email_informado = input("Digite um e-mail válido para receber as CVEs encontradas: ")
-    if (re.search(padrao,email_informado)) :
-        print("Email válido")
-        break
-'''
-
-#software = input('Digite qual a vulnerabilidade que gostaria de procurar: ')
-#startDate_input = input('Informe qual a data de Início: ')
-#startDate_input = '02/01/2022'
-#endDate_input = input('Informe qual a data Final: ')
-#endDate_input = '02/07/2022'
-#email_informato = input("Digite um e-mail válido para receber as CVEs encontradas: ")
-
 from ssl import AlertDescription
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -59,12 +23,12 @@ def homepage():
 def pesquisar():
     error = None
     try:
-
         software_flask = request.form['software']
         email_flask = request.form['email']
         data_inicio_flask = request.form['dataInicio']
         data_termino_flask = request.form['dataTermino']        
 
+        # Recebe a Data da página HTML e converte no formato do site Nist
         datas = validador_datas(data_inicio_flask, data_termino_flask)
         startDate_input = datas[0]
         endDate_input = datas[1]    
@@ -72,7 +36,7 @@ def pesquisar():
         #Parâmetros de Opções do Webdriver do Chrome 
         options = Options()
         options.add_argument('window-size=800,1000')
-        options.add_argument('--headless')
+        #options.add_argument('--headless')
 
         #Definindo o navegador através do Webdriver e inicializando com parâmetros de um objetos da classe "Option"
         navegador = webdriver.Chrome(options=options)
@@ -181,6 +145,8 @@ def pesquisar():
 
         #Envio do Email
         envia_email(listFull,email_flask)
+        
+        return render_template("success.html")
     except:
         #return render_template("homepage.html", error = error)
         pag.alert(text="Por favor, preencha novamente os campos.", title="Erro" )
